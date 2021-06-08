3349300b03f022687af36459f1938626c76566d1
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-06-08 22:42:46 +0530

    uses of specific mapping instead of RequestMapping.

--- course-api-date/src/main/java/com/ritubrata/springbootstarter/course/CourseController.java
+++ course-api-date/src/main/java/com/ritubrata/springbootstarter/course/CourseController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ritubrata.springbootstarter.topic.Topic;
@@ -29,13 +31,13 @@ public class CourseController {
 		return courseService.getCourse(id);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST , value = "/topics/{topicId}/courses")
+	@PostMapping(value = "/topics/{topicId}/courses")
 	public void addCourse(@RequestBody Course course, @PathVariable("topicId") String topicId) {
 		course.setTopic(new Topic(topicId, "", ""));
 		courseService.addCourse(course);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT , value = "/topics/{topicId}/courses/{courseId}")
+	@PutMapping(value = "/topics/{topicId}/courses/{courseId}")
 	public void updateCourse(@RequestBody Course course , 
 			@PathVariable("topicId") String topicId, 
 			@PathVariable("courseId") String id) {
@@ -43,7 +45,7 @@ public class CourseController {
 		courseService.updateCourse(course);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE , value = "/topics/{topicId}/courses/{courseId}")
+	@DeleteMapping(value = "/topics/{topicId}/courses/{courseId}")
 	public void deleteCourse(@PathVariable("courseId") String id) {
 		courseService.deleteCourse(id);
 	}
